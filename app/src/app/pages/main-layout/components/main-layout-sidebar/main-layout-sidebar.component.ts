1ec9b87213600f52ce28c874015927280a32c59f
abiluba9 <abiluba9@gmail.com>
2026-02-06 19:21:21 +0200

    feat: add main layout sidebar component and update main layout structure

---
+++ app/src/app/pages/main-layout/components/main-layout-sidebar/main-layout-sidebar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-layout-sidebar',
+  imports: [],
+  templateUrl: './main-layout-sidebar.component.html',
+  styleUrl: './main-layout-sidebar.component.scss',
+})
+export class MainLayoutSidebarComponent {
+
+}
