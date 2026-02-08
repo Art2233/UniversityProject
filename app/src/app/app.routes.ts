14c7cd7b54510ad5d084debe8e2cfbb536a14a64
abiluba9 <abiluba9@gmail.com>
2026-02-08 13:40:33 +0200

    refactor: remove main layout and example page components, effects, and related styles

--- app/src/app/app.routes.ts
+++ app/src/app/app.routes.ts
@@ -1,14 +1,4 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    {
-        path: '',
-        loadComponent: () => import('./pages/main-layout/main-layout.component').then(m => m.MainLayoutComponent),
-        children: [
-            {
-                path: 'example',
-                loadComponent: () => import('./pages/main-layout/children-pages/example-page/example-page.component').then(m => m.ExamplePageComponent)
-            }
-        ]
-    },
 ];
--- app/src/app/reducer.ts
+++ app/src/app/reducer.ts
@@ -1,11 +1,8 @@
-import { IState as IMainLayoutState, reducer as mainLayoutReducer } from "./pages/main-layout/storage/reducer";
 
 export interface IStore {
-    mainLayout: IMainLayoutState
+
 }
 
 export const reducers = {
-    mainLayout: mainLayoutReducer
-}
 
-// export const { selectCurrentRoute, selectRouteParams } = getRouterSelectors();
+}
